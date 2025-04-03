3a81196f7517ccf31f6595c96da9c344b68f7a4c
hoontaepark <pthomj13@gmail.com>
2025-04-03 17:18:24 +0900

    compnent 추가

--- my-app/src/chapter_03/Book.jsx
+++ my-app/src/chapter_03/Book.jsx
@@ -4,7 +4,7 @@ function Book(props){
     return (
         <div>
             <h1>{`이책의 이름은 ${props.name}입니다.`}</h1> 
-            <h2>{`이책의 이름은 ${props.numOfPage}입니다.`}</h2>
+            <h2>{`이 책은 총 ${props.numOfPage} 페이지로 이뤄져있습니다.`}</h2>
         </div>
     );
 }
