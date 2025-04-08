a76e4724d01a2953b4194c8efcb0674f2ccae7e3
hoontaepark <pthomj13@gmail.com>
2025-04-08 14:55:21 +0900

    comment.jsx 파일 추가

---
+++ my-app/src/chapter_05/CommentList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Comment from "./Comment";
+
+function CommentList(props){
+    return(
+        <div>
+            <Commmnet />
+        </div>
+    );
+}
+
+export default CommentList;
