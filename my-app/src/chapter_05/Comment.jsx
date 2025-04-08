7eec078f4dd8c37aae811e1a4584f8ec2d7e8a39
hoontaepark <pthomj13@gmail.com>
2025-04-08 15:03:59 +0900

    style추가

--- my-app/src/chapter_05/Comment.jsx
+++ my-app/src/chapter_05/Comment.jsx
@@ -1,5 +1,41 @@
 import React from "react";
 
+const style = {
+    wrapper:{
+        margin: 8,
+        padding: 8,
+        display: "flex",
+        flexDirection:"row",
+        border: "1px solid black",
+        borderRadius : 16,
+    },
+    imageContainer:{
+
+    },
+    image:{
+        width: 50,
+        height: 50,
+        borderRaduis: 25,
+    },
+    contenContainer:{
+        marginLeft:8,
+        display:"flex",
+        flexDirection:"column",
+        justifyContent:"center",
+    },
+    nameText:{
+        color:"black",
+        fontSize:16,
+        fontWeight: "bold",
+    },
+    commentText:{
+        color:"black",
+        fontSize: 16,
+    },
+};
+
+
+
 function Comment(props){
     return(
         <div>
